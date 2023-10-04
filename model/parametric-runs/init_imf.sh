31bfead44920f3e97245281b46fd3bfbf4ce9581
rongxinyin <rongxinyin.com>
2023-10-04 13:00:26 -0700

    first batch run framework

---
+++ model/parametric-runs/init_imf.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Define the input CSV file and the output text file
+input_file="parametric-runs-input.csv"
+# output_file="init.imf"
+
+# Define the sublevel folder to save the output files
+output_folder="init_files"
+
+# Create the sublevel folder if it does not exist
+mkdir -p "$output_folder"
+
+# Initialize the output file
+# echo "" > "$output_file"
+
+# Read the CSV file line by line
+awk -F ',' '
+NR==1 {
+    # Store the field names from the first line
+    for (i=1; i<NF; i++) {
+        gsub(/"/, "");
+        field_names[i]=$i;
+    }
+    next;
+}
+{
+    # Initialize the output file
+    output_file="'$output_folder'/init_" NR-1 ".imf"
+    print "" > output_file
+
+    # add first line to the init file
+    print "! ----parameters----" >> output_file;
+
+    # For each line, write each field name and value into the output file
+    for (i=1; i<NF; i++) {
+        print "##set1 " field_names[i] "[] " $i >> output_file;
+    }
+
+    # Include the parameter imf file
+    print "##include parameter.imf" >> output_file;
+
+    # Add an empty line between records
+    print "" >> output_file;
+}' "$input_file"
---
+++ model/parametric-runs/run_imf.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Run the baseline model
+
+ERR="err.log"
+
+# Define the sublevel folder to save the output files
+input_folder="init_files"
+output_folder="output_files"
+
+# Create the sublevel folder if it does not exist
+mkdir -p "$output_folder"
+
+for FILE in $input_folder/init_*.imf
+do
+	echo "Processing $FILE"
+	if [ -f "${FILE%%.*}.epmdet" ];
+	then
+	   echo "File $FILE exist and cleanup Old $FILE" >$ERR
+	   # Delete all output files
+	   rm ${FILE%%.*}.epmdet
+	   rm ${FILE%%.*}.epmidf
+	   rm ${FILE%%.*}.idf
+	else
+	   runepmacro ${FILE%%.*}
+	   mv ${FILE%%.*}.epmidf ${FILE%%.*}.idf
+	   rm ${FILE%%.*}.epmdet
+	   energyplus -w ../weather-files/IDN_JAKARTA-SOEKARNO-HA_967490_IW2/IDN_JAKARTA-SOEKARNO-HA_967490_IW2.EPW -r ${FILE%%.*}.idf 
+	   echo "Run File $FILE Model." >$ERR
+
+	   mv eplusout.csv $output_folder/$(basename ${FILE%%.*}.csv)
+	   mv eplustbl.htm $output_folder/$(basename ${FILE%%.*}.htm)
+	   rm eplus*
+	   rm sqlite.err
+	fi
+done
